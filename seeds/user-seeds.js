448f479a3289634baeae7e0345efdc2c2eabc8e2
elystanz <elysestanziale@gmail.com>
2022-05-31 15:38:12 -0500

    change spelling error in user-seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -22,6 +22,6 @@ const userData = [
 
 ];
 
-const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
+const seedUsers = () => User.bulkCreate(userData, {individualHooks: true});
 
 module.exports = seedUsers;
