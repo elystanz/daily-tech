6e1d2ef5c7558be6e0fc8a4be2826308c80128a4
elystanz <elysestanziale@gmail.com>
2022-07-20 23:16:30 -0400

    change connections for heroku push

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,48 @@
-const Sequelize = require('sequelize')
+// const Sequelize = require('sequelize')
 
+// require('dotenv').config();
+
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PW,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306,
+//     },
+//   );
+// }
+
+// module.exports = sequelize;
+
+'use strict';
+
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
+const DB = 'users';
+const USER = 'user';
+const PASSWORD = 'password';
+const HOST = 'host';
+const DIALECT = 'mysql';
+const PORT = 5432;
+
+const sequelize = new Sequelize(
+    DB,
+    USER, 
+    PASSWORD, 
     {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    },
-  );
-}
+        host: HOST,
+        dialect: DIALECT,
+        port: PORT,
+    }
+)
 
-module.exports = sequelize;
+module.exports.sequelize = sequelize;
